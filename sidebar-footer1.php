81c6b2591ad9f711df170481185a8944c50cb805
ghebre ghebremedhin <geberhane@gmail.com>
2015-01-01 22:56:06 -0800

    adding some files

---
+++ sidebar-footer1.php
@@ -0,0 +1,10 @@
+ 
+				<div id="inner-footer" class="clearfix"> 
+		          <div id="widget-footer" class="clearfix row span4">
+		            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer1') ) : ?>
+		            <?php endif; ?> 
+		          </div> 
+				
+				</div> <!-- end #inner-footer -->
+				
+		 
